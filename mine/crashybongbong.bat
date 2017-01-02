0a875d89d37341fd6839d6212adf7dce25e7226d
KDiaCoding <KDiaCoding@users.noreply.github.com>
2017-01-02 15:24:44 +0100

    Update crashybongbong.bat

--- mine/crashybongbong.bat
+++ mine/crashybongbong.bat
@@ -1,1 +1,17 @@
-@echo offtitle boicolor 1a:startecho ================echo.echo welcome my doodecho.echo ================pauseclsecho are doing well?set /p boi=if %boi%==good goto goodif %boi%==bad goto bad:goodecho really? Well then....Clsecho i guess ill ruin it then.goto crash:crashcolor 0c%0goto crash:badecho oping localhost -n .15 >nulclsecho ohping localhost -n .15 >nulclsecho oh rping localhost -n .15 >nulclsecho oh reping localhost -n .15 >nulclsecho oh reaping localhost -n .15 >nulclsecho oh realping localhost -n .15 >nulclsecho oh reallping localhost -n .15 >nulclsecho oh reallyping localhost -n .15 >nulclsecho oh really.ping localhost -n .15 >nulclsecho oh really..ping localhost -n .15 >nulclsecho oh really...ping localhost -n .15 >nulclsTIMEOUT 5echo well then, iping localhost -n .15 >nulclsecho well then, i gping localhost -n .15 >nulclsecho well then, i guping localhost -n .15 >nulclsecho well then, i gueping localhost -n .15 >nulclsecho well then, i guesping localhost -n .15 >nulclsecho well then, i guessping localhost -n .15 >nulclsecho well then, i guess iping localhost -n .15 >nulclsecho well then, i guess iping localhost -n .15 >nulclsecho well then, i guess iping localhost -n .15 >nulclsecho well then, i guess ill ruin itTIMEOUT 5goto crashy
+@echo off
+title boi
+color 1a
+:start
+echo ================
+echo.
+echo welcome my dood
+echo.
+echo ================
+pause
+cls
+echo are doing well?
+set /p boi=
+if %boi%==good goto good
+if %boi%==bad goto bad:goodecho really? Well then....Clsecho i guess ill ruin it then.goto crash:crashcolor 0c%0goto crash:badecho oping localhost -n .15 >nulclsecho ohping localhost -n .15 >nulclsecho oh rping localhost -n .15 >nulclsecho oh reping localhost -n .15 >nulclsecho oh reaping localhost -n .15 >nulclsecho oh realping localhost -n .15 >nulclsecho oh reallping localhost -n .15 >nulclsecho oh reallyping localhost -n .15 >nulclsecho oh really.ping localhost -n .15 >nulclsecho oh really..ping localhost -n .15 >nulclsecho oh really...ping localhost -n .15 >nulclsTIMEOUT 5echo well then, iping localhost -n .15 >nulclsecho well then, i gping localhost -n .15 >nulclsecho well then, i guping localhost -n .15 >nulclsecho well then, i gueping localhost -n .15 >nulclsecho well then, i guesping localhost -n .15 >nulclsecho well then, i guessping localhost -n .15 >nulclsecho well then, i guess iping localhost -n .15 >nulclsecho well then, i guess iping localhost -n .15 >nulclsecho well then, i guess iping localhost -n .15 >nulclsecho well then, i guess ill ruin itTIMEOUT 5goto crashy
+
+
